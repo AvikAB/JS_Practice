4c3ce477018bf313141ef4d43d38902d638e1066
Avik <119071924+AvikAB@users.noreply.github.com>
2026-06-04 16:33:32 +0600

    Create ultra_basics.js

---
+++ ultra_basics.js
@@ -0,0 +1,10 @@
+/*
+1. The DOM
+2. Event & Event Handling
+3. Form & Form Validations
+4. Timers & Intervals
+5. localStorage, sessionStorage & cookies
+6. Practice
+*/
+
+// DOM: Document Object Model
